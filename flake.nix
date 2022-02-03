16d0e4499c0716b537d677c7d6d2c8e9ced7f329
Gabriel Volpe <volpegabriel@gmail.com>
2022-02-03 14:38:32 +0100

    nix flakes: migration

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Scala development shell";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    flake-compat = {
+      url = github:edolstra/flake-compat;
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      forSystem = system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          jdk = pkgs.jdk17_headless;
+        in
+        {
+          devShell = pkgs.mkShell {
+            name = "scala-dev-shell";
+            buildInputs = [
+              jdk
+              pkgs.coursier
+              pkgs.gnupg
+              pkgs.sbt
+            ];
+            shellHook = ''
+              JAVA_HOME="${jdk}"
+            '';
+          };
+        };
+    in
+    flake-utils.lib.eachDefaultSystem forSystem;
+}
--- shell.nix
+++ shell.nix
@@ -1,35 +1,10 @@
-{ jdk ? "jdk17" }:
-
-let
-  config = {
-    packageOverrides = p: rec {
-      java = p.${jdk};
-
-      sbt = p.sbt.overrideAttrs (
-        old: rec {
-          patchPhase = ''
-            echo -java-home ${java} >> conf/sbtopts
-          '';
-        }
-      );
-    };
-  };
-
-  nixpkgs = fetchTarball {
-    name   = "nixos-unstable-2021-10-27";
-    url    = "https://github.com/NixOS/nixpkgs/archive/a4bf44345706.tar.gz";
-    sha256 = "0zag9yfqsf544vrfccfvn5yjagizqf69adza8fpmsmn5ll8jw8gw";
-  };
-
-  pkgs = import nixpkgs { inherit config; };
-in
-pkgs.mkShell {
-  name = "scala-shell";
-
-  buildInputs = [
-    pkgs.coursier
-    pkgs.gnupg
-    pkgs.${jdk}
-    pkgs.sbt
-  ];
-}
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = ./.; }
+).shellNix
