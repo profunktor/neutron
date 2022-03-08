d3b8cdb2112c462a16e949659d7d7bca50b5a99d
Scala Steward <me@scala-steward.org>
2022-03-08 10:04:13 +0100

    Update cats-effect to 3.3.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val avro4s_2   = "4.0.12"
     val avro4s_3   = "5.0.0.M1"
     val cats       = "2.7.0"
-    val catsEffect = "3.3.6"
+    val catsEffect = "3.3.7"
     val circe      = "0.14.1"
     val fs2        = "3.2.5"
     val pulsar     = "2.9.1"
