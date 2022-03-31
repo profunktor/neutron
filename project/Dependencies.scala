9ccb1cf2bb33a5581645805d9f52c123f6910368
Scala Steward <me@scala-steward.org>
2022-03-31 10:49:03 +0200

    Update pulsar-client, ... to 2.9.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val catsEffect = "3.3.9"
     val circe      = "0.14.1"
     val fs2        = "3.2.5"
-    val pulsar     = "2.9.1"
+    val pulsar     = "2.9.2"
     val weaver     = "0.7.11"
 
     val kindProjector   = "0.13.2"
