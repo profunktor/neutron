43aae6134f91da27d29ef99e58b3ac2efa9ae6a7
Scala Steward <me@scala-steward.org>
2022-05-23 12:47:59 +0200

    Update weaver-cats, weaver-scalacheck to 0.7.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val circe      = "0.14.2"
     val fs2        = "3.2.7"
     val pulsar     = "2.10.0"
-    val weaver     = "0.7.11"
+    val weaver     = "0.7.12"
 
     val kindProjector   = "0.13.2"
     val organizeImports = "0.5.0"
